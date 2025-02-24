23b68d49aca247573cbc8eef86b406fccdd0195e
IC4666 <imonchowdhury4666@gmail.com>
2025-02-24 21:01:29 +0600

    Updated CakeShop folder

--- JavaScript-HTML-CSS/CakeShop/script.js
+++ JavaScript-HTML-CSS/CakeShop/script.js
@@ -4,15 +4,16 @@ let sections = document.querySelectorAll('section');
 let nav = document.querySelectorAll('.nav-placeholder a');
 
 window.onscroll = () => {
+    let top = window.scrollY;
     sections.forEach(sec => {
-        let top = window.scrollY;
+
         let offset = sec.offsetTop - 200;
         let height = sec.offsetHeight;
 
         if (top >= offset && top < offset + height) {
 
             sec.classList.add('show-animation')
-            
+
             nav.forEach(link => {
                 link.classList.remove('clicked'); // Remove clicked class from all
                 // Check if the link corresponds to the current section
@@ -20,8 +21,7 @@ window.onscroll = () => {
                     link.classList.add('clicked'); // Add clicked class to the active link
                 }
             });
-                    
-           
+
         }
         else {
             sec.classList.remove('show-animation')
@@ -30,11 +30,29 @@ window.onscroll = () => {
     })
 }
 
+// Read more button
+function readMore() {
+
+    let text = document.querySelector('.page-2 p span');
+    let btn = document.querySelector('.page-2 button');
+
+    if (btn.innerHTML === "Show more") {
+        text.style.display = 'inline';
+        btn.innerHTML = "Show less";
+
+    }
+    else {
+        text.style.display = 'none';
+        btn.innerHTML = "Show more";
+    }
+
+}
+
 
 // nav.forEach(link => {
 //     nav.addEventListener('click', function() {
 //         nav.forEach(l => l.classList.remove('clicked'));
 //         this.classList.add('clicked');
-        
 //     });
 // });
+
