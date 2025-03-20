953582b97577af7e46fdd2776b239139959cda9b
IC4666 <imonchowdhury4666@gmail.com>
2025-03-21 00:17:03 +0600

    created a new js project in this directory called product-list

---
+++ html-css-javascript/product-display/script.js
@@ -0,0 +1,39 @@
+let productHtml = "";
+products.forEach((product) => {
+    productHtml += `
+    <div class="product">
+        <div class="product-image">
+            <img src="images/${product.image}" alt="${product.name}" title="${product.name}">
+        </div>
+        <div class="product-details">
+            <h3>${product.name}</h3>
+            <p>Price: ${product.price} taka</p>
+            <p>Color: ${product.color.join(", ")}</p>
+            <p>Brand: ${product.brand}</p>
+            <p>Discount: ${product.discount}%</p>
+            <p>Stock: ${product.stock}</p>
+            <button class="product-button">Buy Now</button>
+        </div>
+   </div>`;
+});
+
+document.querySelector(".product-container").innerHTML = productHtml;
+
+
+
+document.querySelectorAll(".product-button").forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+
+        popupBox = document.querySelector(".pop-up");
+        popupText = document.querySelector(".pop-up-text");
+
+        popupText.innerText = "Product number (" + (index + 1) + ") is added to cart";
+        popupBox.showModal();
+
+        setTimeout(() => {
+            popupBox.close();
+        }, 1000);
+    })
+});
+
+
