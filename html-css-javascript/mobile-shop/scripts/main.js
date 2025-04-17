a88183f6d6ee82e461cf1b484c7062273a1edd11
IC4666 <imonchowdhury4666@gmail.com>
2025-04-17 13:51:52 +0600

    refactor: update cart functionality and improve product display logic

--- html-css-javascript/mobile-shop/scripts/main.js
+++ html-css-javascript/mobile-shop/scripts/main.js
@@ -1,13 +1,14 @@
 import { products } from "./product-data.js";
-import { cart } from "./cart.js";
+import { cart, addToCart } from "./cart.js";
 
+//  Displaying the products on the page
 let productHtml = "";
 products.forEach((product) => {
     document
     productHtml += `
     <div class="product-card blur-animation">
         <div class="product-image">
-            <img src="images/${product.image}" alt="${product.name}" title="${product.name}">
+            <img src="images/${product.image}" alt="${product.name}" title="${product.name}" loading="lazy">
         </div>
         <div class="product-details">
             <h3>${product.name}</h3>
@@ -16,19 +17,18 @@ products.forEach((product) => {
             <p>Brand: ${product.brand}</p>
             <p>Discount: ${product.discount}%</p>
             <p>Stock: ${product.stock}</p>
-
             <button class="product-button" data-product-id = ${product.id}>Add to cart</button>
-
         </div>
    </div>`;
 });
 
 document.querySelector(".product-container").innerHTML = productHtml;
 
-//  This is a pop-up
-let addToCart = document.querySelectorAll(".product-button");
 
-addToCart.forEach((btn, index) => {
+let addToCartBtn = document.querySelectorAll(".product-button");
+
+//  This is a pop-up
+addToCartBtn.forEach((btn, index) => {
     btn.addEventListener("click", () => {
 
         let popupBox = document.querySelector(".pop-up");
@@ -43,44 +43,31 @@ addToCart.forEach((btn, index) => {
     })
 });
 
-let cartQuantity = document.querySelector(".cart-quantity");
 
-addToCart.forEach((btn) => {
+// calculating and adding cart quantity to the cart icon
+let DisplayCartQuantity = document.querySelector(".cart-quantity");
+
+if (DisplayCartQuantity.innerText == 0) {
+    DisplayCartQuantity.style.display = "none";
+}
+
+addToCartBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
-        // let productId = btn.dataset.productId;
-       let productId = btn.getAttribute("data-product-id");
+        let productId = btn.getAttribute("data-product-id");
+        //can also be write like this let productId = btn.dataset.productId;
 
-        let matchingItem;
-        cart.forEach((item) => {
-            if (productId === item.productId) {
-                matchingItem = item;
-            }
-        })
-        if (matchingItem) {
-            matchingItem.quantity += 1;
-        } else {
-            cart.push({
-                productId: productId,
-                quantity: 1
-            });
-        }
+        addToCart(productId);
 
         let totalCartQuantity = 0;
-
         cart.forEach((item) => {
             totalCartQuantity += item.quantity;
         })
 
- 
-        cartQuantity.innerText = totalCartQuantity;
-        cartQuantity.style.display = "block";
-        console.log(cart);
+        DisplayCartQuantity.innerText = totalCartQuantity;
+        DisplayCartQuantity.style.display = "block";
     })
 });
 
-if (cartQuantity.innerHTML == 0) {
-    cartQuantity.style.display = "none";
-}
 
 
 
